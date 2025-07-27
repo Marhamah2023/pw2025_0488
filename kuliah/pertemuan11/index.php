fe2efffbeeeacf198b016b790695bb979856dc59
Marhamah2023 <marhamah2002@gmail.com>
2025-07-27 21:10:55 +0700

    menambahkan searching data

--- kuliah/pertemuan11/index.php
+++ kuliah/pertemuan11/index.php
@@ -1,6 +1,11 @@
 <?php
     require 'functions.php';
     $mahasiswa = query("SELECT * FROM mahasiswa");
+
+    //ketika tombol cari diklik
+    if(isset($_POST['cari'])) {
+        $mahasiswa = cari($_POST['keyword']);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +21,12 @@
     <a href="tambah.php">tambah data mahasiswa</a>
     <br><br>
 
+    <form action="" method="post">
+        <input type="text" name="keyword" size="40" placeholder="masukkan keyword pencarian.." autocomplete="off" autofocus>
+        <button type="submit" name="cari">cari</button>
+    </form>
+    <br>
+
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
             <th>#</th>
@@ -23,6 +34,14 @@
             <th>nama</th>
             <th>aksi</th>
         </tr>
+
+        <?php if(empty($mahasiswa)) : ?>
+            <tr>
+                <td colspan="4">
+                    <p style="color: red; font-style: italic;">data mahasiswa tidak ditemukan</p>
+                </td>
+            </tr>
+        <?php endif; ?>
 
         <?php $i = 1; foreach($mahasiswa as $m) : ?>
         <tr>
